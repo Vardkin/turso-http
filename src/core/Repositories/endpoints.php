0b2e5b3cb390bdfae5c53197e605532b5e1a7308
Vardkin <kaseywright7@gmail.com>
2024-03-10 13:16:48 -0400

    Add the ability to use a development url for endpoint APIs

--- src/core/Repositories/endpoints.php
+++ src/core/Repositories/endpoints.php
@@ -1,12 +1,12 @@
 <?php
 
-$baseURL = 'https://api.turso.tech/v1';
+// $baseURL = 'https://api.turso.tech/v1';
 
 return [
-    'tokens' => require_once 'tokens.php',
-    'databases' => require_once 'database.php',
-    'groups' => require_once 'groups.php',
-    'locations' => require_once 'locations.php',
+    'tokens'        => require_once 'tokens.php',
+    'databases'     => require_once 'database.php',
+    'groups'        => require_once 'groups.php',
+    'locations'     => require_once 'locations.php',
     'organizations' => require_once 'organizations.php',
-    'audit_logs' => require_once 'audit_logs.php'
+    'audit_logs'    => require_once 'audit_logs.php'
 ];
